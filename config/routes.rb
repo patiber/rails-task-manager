35520b3584a5ecffd23006c15e4fb2782f86f4e8
Patricia Bertran <pbertranc@gmail.com>
2023-08-15 12:43:04 +0200

    rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update", as: :update
+  delete "tasks/:id", to: "tasks#destroy"
+
 end
